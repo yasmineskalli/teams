8a8ae2975f654ef87634838b6a70c7e88099cd9d
Sprouse1018 <57916422+Sprouse1018@users.noreply.github.com>
2020-01-30 14:04:39 -0500

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,1 +1,26 @@
-
+roomArray = []
+for i in range(999):
+    roomArray.append(False)
+#French room
+roomArray[203] = ""
+roomArray[303] = ""
+roomArray[204] = ""
+roomArray[304] = ""
+roomArray[403] = ""
+#Hallway
+roomArray[503] = ""
+roomArray[504] = ""
+roomArray[505] = ""
+roomArray[506] = ""
+#Computer lab
+roomArray[604] = ""
+roomArray[704] = ""
+roomArray[804] = ""
+roomArray[705] = ""
+roomArray[805] = ""
+#Engineering room
+roomArray[206] = ""
+roomArray[306] = ""
+roomArray[406] = ""
+roomArray[207] = ""
+roomArray[307] = ""
