458eeb28b1bf2eeb537ef1d8c000c4ca9d3bff65
Sprouse1018 <57916422+Sprouse1018@users.noreply.github.com>
2020-02-05 14:18:09 -0500

    Update teams.py

--- teams.py
+++ teams.py
@@ -54,77 +54,76 @@ def main():
     print("by Kye, Yasmine, Miri, Kyle")
     sleep(1)
     while True:
+        itemArray[805] = "LegoBot"
+        itemArray[704] = "Keyboard"
+        itemArray[503] = "iPad"
+        itemArray[505] = "Poster"
+        itemArray[303] = "Baguette"
+        itemArray[203] = "Eiffel Tower"
+        itemArray[204] = "Beret"
+        itemArray[304] = "Bidet"
+        itemArray[306] = "Wrench"
+        itemArray[307] = "Robot Arm"
+
+        inventoryArray = []
+        if location == 805:
+            print("do you want to pick up LegoBot? n or y?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("LegoBot")
+
+        if location == 704:
+            print("do you want to pick up the Keyboard? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Keyboard")
+
+        if location == 503:
+            print("do you want to pick up the iPad? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("iPad")
+        if location == 505:
+            print("do you want to pick up the poster? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("poster")
+
+        if location == 303:
+            print("do you want to pick up the Baguette? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Baguette")
+
+        if location == 203:
+            print("do you want to pick up the Eiffel Tower? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Eiffel Tower")
+
+        if location == 204:
+            print("do you want to pick up the Beret? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Beret")
+
+        if location == 304:
+            print("do you want to pick up the Bidet? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Bidet")
+
+        if location == 306:
+            print("do you want to pick up the Wrench? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Wrench")
+
+        if location == 307:
+            print("do you want to pick up the Robot Arm? y or n?")
+            answer = input()
+            if answer == "y":
+                inventoryArray.append("Robot Arm")
         print(str(roomArray[location]))
         userInput = input("Please type: n, s, e, w, or quit: ")
         location = move(userInput, location)
-
-itemArray[805] = "LegoBot"
-itemArray[704] = "Keyboard"
-itemArray[503] = "iPad"
-itemArray[505] = "Poster"
-itemArray[303] = "Baguette"
-itemArray[203] = "Eiffel Tower"
-itemArray[204] = "Beret"
-itemArray[304] = "Bidet"
-itemArray[306] = "Wrench"
-itemArray[307] = "Robot Arm"
-
-inventoryArray = []
-if location == 805:
-    print("do you want to pick up LegoBot? n or y?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("LegoBot")
-
-if location == 704:
-    print("do you want to pick up the Keyboard? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Keyboard")
-
-if location == 503:
-    print("do you want to pick up the iPad? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("iPad")
-if location == 505:
-    print("do you want to pick up the poster? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("poster")
-
-if location == 303:
-    print("do you want to pick up the Baguette? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Baguette")
-
-if location == 203:
-    print("do you want to pick up the Eiffel Tower? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Eiffel Tower")
-
-if location == 204:
-    print("do you want to pick up the Beret? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Beret")
-
-if location == 304:
-    print("do you want to pick up the Bidet? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Bidet")
-
-if location == 306:
-    print("do you want to pick up the Wrench? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Wrench")
-    
-if location == 307:
-    print("do you want to pick up the Robot Arm? y or n?")
-    answer = input()
-    if answer == "y":
-        inventoryArray.append("Robot Arm")
